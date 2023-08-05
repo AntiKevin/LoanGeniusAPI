c07316483af7929b6889e81ed3cfdd0fb3ae0bd5
antikevin <Kevin_3467@yahoo.com>
2023-08-05 17:56:26 -0300

    TEST: Testing Loan Read, Update and Delete

--- src/test/java/br/com/loangenius/web/LoanControllerTest.java
+++ src/test/java/br/com/loangenius/web/LoanControllerTest.java
@@ -13,6 +13,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -61,5 +63,60 @@ public class LoanControllerTest {
                 .andReturn();
     }
 
+    @Test
+    public void testGetLoanById() throws Exception {
+        // Create a sample loan object
+        Loan loan = new Loan();
+        loan.setId(1L);
+        loan.setAmount(1000.0);
+        loan.setInstallments(12.0);
+        loan.setInterest(5.0);
+
+        // Mock the behavior of the loanService.getById(id) method
+        when(loanService.listById(1L)).thenReturn(loan);
+
+        // Perform the GET request to the endpoint with the ID 1
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/loan/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+    }
+
+    @Test
+    public void testUpdateLoan() throws Exception {
+        // Create a sample loan object with updated data
+        Loan updatedLoan = new Loan();
+        updatedLoan.setId(1L);
+        updatedLoan.setAmount(2000.0);
+        updatedLoan.setInstallments(24.0);
+        updatedLoan.setInterest(3.5);
+
+        // Mock the behavior of the loanService.update(id, loan) method
+        when(loanService.update(any(Long.class), any(Loan.class))).thenReturn(updatedLoan);
+
+        // Perform the PUT request to the endpoint with the updated loan data
+        String requestJson = objectMapper.writeValueAsString(updatedLoan);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/api/loan/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.amount").value(2000.0))
+                .andExpect(jsonPath("$.installments").value(24.0))
+                .andExpect(jsonPath("$.interest").value(3.5))
+                .andReturn();
+    }
+
+    @Test
+    public void testDeleteLoan() throws Exception {
+        // Perform the DELETE request to the endpoint with the ID 1
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/api/loan/1"))
+                .andExpect(status().isNoContent())
+                .andReturn();
+
+        // Verify that the loanService.delete(id) method was called with the correct ID (1)
+        verify(loanService).delete(1L);
+    }
 
 }
