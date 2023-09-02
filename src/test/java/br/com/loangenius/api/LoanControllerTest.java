f1bd05505614b48b3d246f7e43974e825af79078
antikevin <Kevin_3467@yahoo.com>
2023-09-02 15:46:04 -0300

    TEST: testing loan controllers authenticated

--- src/test/java/br/com/loangenius/api/LoanControllerTest.java
+++ src/test/java/br/com/loangenius/api/LoanControllerTest.java
@@ -2,6 +2,7 @@ package br.com.loangenius.api;
 
 import br.com.loangenius.domain.models.Loan;
 import br.com.loangenius.application.services.LoanService;
+import br.com.loangenius.domain.models.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -32,11 +34,15 @@ public class LoanControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
+    @WithMockUser(username = "testuser", roles = "USER")
     public void testCreateLoan() throws Exception {
+        User userRequest = new User();
+
         Loan loanRequest = new Loan();
         loanRequest.setAmount(1000.0);
         loanRequest.setInstallments(12.0);
         loanRequest.setInterest(5.0);
+        loanRequest.setUser(userRequest);
 
         Loan createdLoan = new Loan();
         createdLoan.setId(1L);
@@ -56,6 +62,7 @@ public class LoanControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "testuser", roles = "USER")
     public void testGetAllLoans() throws Exception {
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/loan"))
                 .andExpect(status().isOk())
@@ -64,18 +71,19 @@ public class LoanControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "testuser", roles = "USER")
     public void testGetLoanById() throws Exception {
-        // Create a sample loan object
+        User userRequest = new User();
+
         Loan loan = new Loan();
         loan.setId(1L);
         loan.setAmount(1000.0);
         loan.setInstallments(12.0);
         loan.setInterest(5.0);
+        loan.setUser(userRequest);
 
-        // Mock the behavior of the loanService.getById(id) method
         when(loanService.listById(1L)).thenReturn(loan);
 
-        // Perform the GET request to the endpoint with the ID 1
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/loan/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -83,13 +91,16 @@ public class LoanControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "testuser", roles = "USER")
     public void testUpdateLoan() throws Exception {
-        // Create a sample loan object with updated data
+        User userRequest = new User();
+
         Loan updatedLoan = new Loan();
         updatedLoan.setId(1L);
         updatedLoan.setAmount(2000.0);
         updatedLoan.setInstallments(24.0);
         updatedLoan.setInterest(3.5);
+        updatedLoan.setUser(userRequest);
 
         // Mock the behavior of the loanService.update(id, loan) method
         when(loanService.update(any(Long.class), any(Loan.class))).thenReturn(updatedLoan);
@@ -105,10 +116,12 @@ public class LoanControllerTest {
                 .andExpect(jsonPath("$.amount").value(2000.0))
                 .andExpect(jsonPath("$.installments").value(24.0))
                 .andExpect(jsonPath("$.interest").value(3.5))
+                .andExpect(jsonPath("$.sacDetailsList").exists())
                 .andReturn();
     }
 
     @Test
+    @WithMockUser(username = "testuser", roles = "USER")
     public void testDeleteLoan() throws Exception {
         // Perform the DELETE request to the endpoint with the ID 1
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/api/loan/1"))
